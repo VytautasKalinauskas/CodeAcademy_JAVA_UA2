7d291370fb50f0c864bdbacb86189bb873cdb832
Jurkenas <36195315+Jurkenas@users.noreply.github.com>
2023-07-04 22:22:34 +0300

    Basic auth example added

--- spring/spring-data-example/src/main/java/lt/codeacademy/springdataexample/SpringDataExampleApplication.java
+++ spring/spring-data-example/src/main/java/lt/codeacademy/springdataexample/SpringDataExampleApplication.java
@@ -2,8 +2,10 @@ package lt.codeacademy.springdataexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class SpringDataExampleApplication {
 
 	public static void main(String[] args) {
---
+++ spring/spring-data-example/src/main/java/lt/codeacademy/springdataexample/configs/AuthConfig.java
@@ -0,0 +1,56 @@
+package lt.codeacademy.springdataexample.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AuthConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/exams**").permitAll() // Everyone can access all /exams EPs
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails student = User
+                .withUsername("student")
+                .password(passwordEncoder().encode("student1"))
+                .roles("STUDENT")
+                .build();
+
+        UserDetails teacher = User
+                .withUsername("teacher")
+                .password(passwordEncoder().encode("teacher1"))
+                .roles("TEACHER")
+                .build();
+
+        UserDetails guest = User
+                .withUsername("guest")
+                .password(passwordEncoder().encode("guest1"))
+                .roles("GUEST")
+                .build();
+
+        UserDetails[] userDetails = new UserDetails[]{student, teacher, guest};
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+
+}
